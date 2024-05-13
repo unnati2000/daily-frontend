cc782bce187d0681f6589384c163411f755c9cd6
unnati <unnati@tessact.com>
2024-05-13 23:39:45 +0530

    add moon rotate animation

--- app/toggle/page.tsx
+++ app/toggle/page.tsx
@@ -4,8 +4,18 @@ import { useState } from "react";
 
 import { motion } from "framer-motion";
 
+import { WiMoonWaningCrescent4 } from "react-icons/wi";
+
+import { useMotionValue, useMotionValueEvent } from "framer-motion";
+
 const Toggle = () => {
   const [toggle, setToggle] = useState(false);
+
+  const x = useMotionValue(0);
+
+  useMotionValueEvent(x, "change", (latest) => {
+    console.log("x changed to", latest);
+  });
 
   return (
     <div className="flex flex-col h-screen w-screen justify-center items-center">
@@ -14,15 +24,24 @@ const Toggle = () => {
         className="border border-red-200 h-16 p-1 rounded-full cursor-pointer w-40 flex flex-col justify-center"
       >
         <motion.div
+          style={{
+            x,
+            background: x.get() <= 45 ? "yellow" : "transparent",
+            transition: "background ease-in-out 1s",
+          }}
           animate={{
             x: toggle ? 96 : 0,
+            rotate: toggle ? 360 : 0,
+
             transition: {
-              duration: 0.5,
+              duration: 1,
               ease: "easeInOut",
             },
           }}
-          className="h-14 w-14 rounded-full border border-white"
-        ></motion.div>
+          className="h-14 w-14 rounded-full border flex items-center justify-start border-white"
+        >
+          {x.get() >= 45 ? <WiMoonWaningCrescent4 size={96} /> : null}
+        </motion.div>
       </div>
     </div>
   );
