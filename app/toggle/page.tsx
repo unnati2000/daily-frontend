8e93f67c3d5060278895078824f7d7c2d026b6f5
unnati <unnati@tessact.com>
2024-05-13 23:48:12 +0530

    add neumorphism effect for dark mode

--- app/toggle/page.tsx
+++ app/toggle/page.tsx
@@ -18,16 +18,21 @@ const Toggle = () => {
   });
 
   return (
-    <div className="flex flex-col h-screen w-screen justify-center items-center">
+    <div className="flex flex-col h-screen w-screen justify-center items-center bg-[#171616]">
       <div
         onClick={() => setToggle(!toggle)}
-        className="border border-red-200 h-16 p-1 rounded-full cursor-pointer w-40 flex flex-col justify-center"
+        style={{
+          boxShadow: "5px 5px 10px #0a0909, -5px -5px 10px #242323",
+          background: "#171616",
+        }}
+        className="h-16 p-1 rounded-full cursor-pointer w-40 flex flex-col justify-center"
       >
         <motion.div
           style={{
             x,
             background: x.get() <= 45 ? "yellow" : "transparent",
             transition: "background ease-in-out 1s",
+            boxShadow: "5px 5px 16px #121111, -5px -5px 16px #1c1b1b",
           }}
           animate={{
             x: toggle ? 96 : 0,
@@ -38,7 +43,7 @@ const Toggle = () => {
               ease: "easeInOut",
             },
           }}
-          className="h-14 w-14 rounded-full border flex items-center justify-start border-white"
+          className="h-14 w-14 rounded-full flex items-center justify-start"
         >
           {x.get() >= 45 ? <WiMoonWaningCrescent4 size={96} /> : null}
         </motion.div>
